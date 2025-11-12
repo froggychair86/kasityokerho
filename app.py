ea88b351b20807e82d1d97f8b2b5a2685b753600
Linnea Tiilikka <linnea.s.tiilikka@gmail.com>
2025-11-12 18:52:30 +0200

    Add new meeting form

--- app.py
+++ app.py
@@ -12,6 +12,25 @@ app.secret_key = config.secret_key
 def index():
     return render_template("index.html")
 
+@app.route("/new_meeting")
+def new_item():
+    return render_template("new_meeting.html")
+
+@app.route("/create_meeting", methods=["POST"])
+def create_meeting():
+    topic = request.form["topic"]
+    description = request.form["description"]
+    date = request.form["date"]
+    start_time = request.form["start_time"]
+    end_time = request.form["end_time"]
+    user_id = session["user_id"]
+
+    sql = """INSERT INTO meetings (topic, description, date, start_time, end_time, user_id)
+             VALUES (?, ?, ?, ?, ?, ?)"""
+    db.execute(sql, [topic, description, date, start_time, end_time, user_id])
+
+    return redirect("/")
+
 @app.route("/register")
 def register():
     return render_template("register.html")
@@ -37,15 +56,18 @@ def create():
 def login():
     if request.method == "GET":
         return render_template("login.html")
-    
+
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        
-        sql = "SELECT password_hash FROM users WHERE username = ?"
-        password_hash = db.query(sql, [username])[0][0]
+
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql, [username])[0]
+        user_id = result["id"]
+        password_hash = result["password_hash"]
 
         if check_password_hash(password_hash, password):
+            session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
         else:
@@ -53,5 +75,6 @@ def login():
 
 @app.route("/logout")
 def logout():
+    del session["user_id"]
     del session["username"]
     return redirect("/")
--- schema.sql
+++ schema.sql
@@ -1,5 +1,14 @@
-CREATE TABLE Users (
+CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     username TEXT UNIQUE,
     password_hash TEXT
+);
+
+CREATE TABLE meetings (
+    id INTEGER PRIMARY KEY,
+    topic TEXT,
+    description TEXT,
+    date DATE,
+    time TIME,
+    user_id INTEGER REFERENCES users
 );
